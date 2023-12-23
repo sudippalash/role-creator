10c5a5904914998d1ceccdaf3a44b0fbdb783be2
Ferdous Anam <ferdous.anam@gmail.com>
2023-12-23 14:50:03 +0600

    module labels added

--- src/resources/views/bootstrap3/show.blade.php
+++ src/resources/views/bootstrap3/show.blade.php
@@ -41,7 +41,7 @@
                             <td>
                                 <div class="custom-control custom-switch">
                                     <input type="checkbox" class="custom-control-input" checked disabled>
-                                    <label class="custom-control-label">{{ ucfirst($module) }}</label>
+                                    <label class="custom-control-label">{{ Str::of($module)->replace('_', ' ')->replace('-', ' ')->title() }}</label>
                                 </div>
                             </td>
                             <td>
@@ -49,7 +49,7 @@
                                     @foreach($moduleArr as $md)
                                         <div class="col-sm-2 custom-control custom-checkbox">
                                             <input class="custom-control-input" type="checkbox" value="{{ $md }}" checked disabled>
-                                            <label class="custom-control-label">{{ str_replace($module, '', $md) }}</label><br>
+                                            <label class="custom-control-label">{{ Str::of($md)->replace($module, '')->trim()->replace('_', ' ')->replace('-', ' ')->title() }}</label><br>
                                         </div>
                                     @endforeach            
                                 </div>
